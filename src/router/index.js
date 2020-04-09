9e86a89cf25a89b0351a3ac22ada12b9558c5f2c
lxy <1415092990@qq.com>
2020-04-10 02:26:15 +0800

    登陆页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../components/Login'
+import Home from '../components/Home'
+/** 导入组件路径**/
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  /** 路径映射**/
+  { path: '/', component: Login },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+//  挂载路由守卫
+router.beforeEach((to, from, next) => {
+//  to代表要访问的路径
+//  form代表从哪个路径来
+//  next是一个函数表示放行
+//  next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  //  获取token
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
